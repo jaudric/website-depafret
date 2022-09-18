fe8a5e72f68753adbb671c174c11b447d9271d04
jaudric <jaurestankeu@gmail.com>
2022-09-18 12:47:34 +0400

    ajout de whatsapp

--- forms/contact.php
+++ forms/contact.php
@@ -1,37 +1,31 @@
 <?php
+ require "vendor/autoload.php";
+ use PHPMailer\PHPMailer\PHPMailer;
+ use PHPMailer\PHPMailer\SMTP;
 
-    $to = "rockybd1995@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+if (!empty($_POST)){
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $mail = new PHPMailer(true);
+	$mail->isSMTP();
+	$mail->SMTPAuth = true;
 
-    $subject = "You have a message from your Bitmap Photography.";
+	$mail->Host = "smtp.ionos.fr";
+	//$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+	$mail->port = 587;
+	$mail->Username = 'contact@depa-fret.fr';
+	$mail->Password = 'Lejourdegloire1';
 
-    $logo = 'img/logo.png';
-    $link = '#';
+	$mail->SetFrom($from, $name);
+	$mail->addAddress("contact@depa-fret.fr", "Service Depa");
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-    $send = mail($to, $subject, $body, $headers);
-
+	$mail->Subject =  $subject;
+	$mail->Body = $message;
+	$mail->Send();
+	
+	//header("Location: ../sent.html");
+}
 ?>
